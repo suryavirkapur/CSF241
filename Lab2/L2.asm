311d489e27424f4189ca6b23c7dfbe50e22282a8
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2024-02-23 10:43:42 +0400

    Create L2.asm

---
+++ Lab2/L2.asm
@@ -0,0 +1,30 @@
+; SURYAIVR KAPUR
+; 2022A7PS0293U
+.MODEL SMALL
+.STACK 20
+.DATA
+ORG 1000H
+N1 DB 35H
+N2 DB 82H
+Un_Sign_PROD DW ?
+Sign_PROD DW ?
+
+.CODE 
+START:
+MOV AX,@DATA
+MOV DS,AX
+MOV DX, 0
+
+MOV AX,N1
+DIV N2
+MOV Un_Sign_PROD,AX
+
+
+MOV AX,N1
+IDIV N2
+MOV Sign_PROD, AX
+
+
+INT 3
+END START
+; After the division, the quotient is in AX and the remainder is in DX
